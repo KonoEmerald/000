f4541ad48af1ed2f231a709c9b58c9ea9e916d3f
鈴木美優 <miyu@suzukimiyuus-MacBook-Air.local>
2022-11-30 22:53:55 +0900

    add header and footer

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
 
-// app.use(express.static(public))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.render('test.ejs');
+	res.render('progress.ejs');
 });
 
 app.get('/login', (req, res) => {
 	res.render('login.ejs');
-})
+});
 
 app.get('/register', (req, res) => {
 	res.render('register.ejs');
-})
+});
+
+app.get('/test', (req, res) => {
+	res.render('test.ejs');
+});
 
 app.listen(3000);
